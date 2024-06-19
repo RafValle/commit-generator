0a58abe515096cb7d00e66469ba1203ac85f980a
Rafael Leonardo Valle Pinheiro <rafael.pinheiro@grupomateus.com>
2024-06-19 16:45:03 -0300

    ajuste

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,15 +12,17 @@ export class FormComponent {
   workType: string = '';
   outputBranch: string | undefined;
   outputCommit: string | undefined;
-  clickCount: number = 0; // Contador de cliques
+  clickCount: number = 0; // Contador de cliques do botão
+  imageClickCount: number = 0; // Contador de cliques da imagem
   showGif: boolean = false; // Controla a exibição do GIF
+  gifUrl: string = 'https://steamuserimages-a.akamaihd.net/ugc/545258778418733357/190047F53F0BD2E3C8590CF812CEF065CF40AF83/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'; // URL do GIF inicial
 
   onSubmit() {
-    this.clickCount++; // Incrementa o contador de cliques
+    this.clickCount++; 
     if (this.clickCount >= 5) {
-      this.showGif = true; // Exibe o GIF após 5 cliques
+      this.showGif = true; 
     } else {
-      this.showGif = false; // Oculta o GIF se os cliques forem menos de 5
+      this.showGif = false; 
     }
 
     if (this.cardNumber && this.squad && this.cardTitle && this.workType) {
@@ -28,11 +30,23 @@ export class FormComponent {
       this.outputBranch = `${this.workType}-GMS-ERPSquad${this.squad}-${this.cardNumber}-${formattedTitle.toLowerCase()}`;
       this.outputCommit = `feat(${this.squad}): [GMS-ERP\\Squad ${this.squad} ${this.cardNumber}] - ${this.cardTitle}`;
     } else {
+      this.clickCount++; 
       if (this.clickCount >= 5) {
-        this.showGif = true; // Exibe o GIF após 5 cliques
+        this.showGif = true; 
       } else {
-        this.showGif = false; // Oculta o GIF se os cliques forem menos de 5
-      }    
+        this.showGif = false; 
+      }    }
+  }
+
+  onImageClick() {
+    this.imageClickCount++; 
+    if (this.imageClickCount >= 5) {
+      this.imageClickCount = 0; 
+      this.changeGif(); 
     }
+  }
+
+  changeGif() {
+    this.gifUrl = 'https://hentaidude.tv/wp-content/uploads/2022/01/KyonyuuElfOyakoSaimin-Episode1-Omake-4.gif';
   }
 }
