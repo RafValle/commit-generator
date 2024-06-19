51e9a8733712b418e95562987cab2a38d5e77a72
Rafael Leonardo Valle Pinheiro <rafael.pinheiro@grupomateus.com>
2024-06-19 14:22:31 -0300

    ajuste

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,12 +12,21 @@ export class FormComponent {
   workType: string = '';
   outputBranch: string | undefined;
   outputCommit: string | undefined;
+  clickCount: number = 0; // Contador de cliques
+  showGif: boolean = false; // Controla a exibição do GIF
 
   onSubmit() {
+    this.clickCount++; // Incrementa o contador de cliques
+    if (this.clickCount >= 5) {
+      this.showGif = true; // Exibe o GIF após 5 cliques
+    } else {
+      this.showGif = false; // Oculta o GIF se os cliques forem menos de 5
+    }
+
     if (this.cardNumber && this.squad && this.cardTitle && this.workType) {
       const formattedTitle = this.cardTitle.split(' ').join('-');
       this.outputBranch = `${this.workType}-GMS-ERPSquad${this.squad}-${this.cardNumber}-${formattedTitle.toLowerCase()}`;
-      this.outputCommit = `${this.workType}(${this.squad}): [GMS-ERP\\Squad ${this.squad} ${this.cardNumber}] - ${this.cardTitle}`;
+      this.outputCommit = `feat(${this.squad}): [GMS-ERP\\Squad ${this.squad} ${this.cardNumber}] - ${this.cardTitle}`;
     } else {
       alert('Por favor, preencha todos os campos.');
     }
